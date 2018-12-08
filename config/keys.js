c31c2f09f3c6d15b8307a727bc0b838e68a3689f
Eddie Borja <eddie@Eddies-MBP.frontierlocal.net>
2018-12-07 17:49:35 -0800

    adding in server config

---
+++ config/keys.js
@@ -0,0 +1,4 @@
+module.exports = {
+    googleClientID: '760236109627-d3b384d0b4nv0v547atv10p8vmjcb4ns.apps.googleusercontent.com',
+    googleClientSecret: 'LGISNmJLyEjBhAFpBXBIkiEp'
+};
--- index.js
+++ index.js
@@ -1,9 +1,27 @@
 const express = require('express');
 const app = express();
+const passport = require('passport');
+const GoogleStrategy = require("passport-google-oauth20").Strategy;
+const keys = require('./config/keys');
 
-app.get('/', (req, res) => {
-    res.send({ Hi: 'ALLALALALLALALA'})
-});
+passport.use(new GoogleStrategy({
+        clientID: keys.googleClientID,
+        clientSecret: keys.googleClientSecret,
+        callbackURL: '/auth/google/callback'
+    },
+        accessToken => {
+        console.log(accessToken);
+    })
+);
+
+app.get('/auth/google', passport.authenticate('google', {
+    scope: ['email', 'profile']
+    })
+);
+
+// app.get('/', (req, res) => ∏
+//     res.send({ Hi: 'ALLALALALLALALA'})
+// });
 
 const PORT = process.env.PORT || 5000;
 
