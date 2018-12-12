99631b1ae46eda54caf51d3bf8ad017e4f10a66f
Eddie Borja <eddie@Eddies-MBP.frontierlocal.net>
2018-12-11 19:27:37 -0800

    setting up passport to create a new user if one doesnt exist

--- Services/passport.js
+++ Services/passport.js
@@ -1,7 +1,9 @@
 const passport = require('passport');
 const GoogleStrategy = require("passport-google-oauth20").Strategy;
+const mongoose = require('mongoose')
 const keys = require('../config/keys');
 
+const User = mongoose.model('users');
 
 passport.use(new GoogleStrategy({
     clientID: keys.googleClientID,
@@ -9,8 +11,21 @@ passport.use(new GoogleStrategy({
     callbackURL: '/auth/google/callback'
 },
     (accessToken, refreshToken, profile,  done) => {
-    console.log(accessToken);
-    console.log(refreshToken);
-    console.log(profile);
+        User.findOne({googleId: profile.id}).then((existingUser) => {
+            if (existingUser) {
+                // we already have a user with this ID
+                done(null, existingUser);
+            } else {
+                // we dont have a record for this ID
+                new User({googleId: profile.id})
+                    .save()
+                    .then(user => done(null, user));
+            }
+        })
+
+        
+    // console.log("AccessToken " , accessToken);
+    // console.log("RefreshToken " , refreshToken);
+    // console.log("profile", profile);
     })
 );
