ffb29efd22924823c042e55aaaa7874dc4d8006e
Eddie Borja <eddie@Eddies-MacBook-Pro.local>
2018-12-20 19:08:44 -0800

    added in cookies for authentication

--- config/keys.js
+++ config/keys.js
@@ -1,5 +1,6 @@
 module.exports = {
     googleClientID: '760236109627-d3b384d0b4nv0v547atv10p8vmjcb4ns.apps.googleusercontent.com',
     googleClientSecret: 'LGISNmJLyEjBhAFpBXBIkiEp',
-    mongoURI: 'mongodb://eddiebor:2015subarusti@ds035897.mlab.com:35897/feedbackapp-dev'
+    mongoURI: 'mongodb://eddiebor:2015subarusti@ds035897.mlab.com:35897/feedbackapp-dev',
+    cookieKey: 'alsjkdlfjalsdjfalsjdfiowejfowonevnwoenow'
 };
